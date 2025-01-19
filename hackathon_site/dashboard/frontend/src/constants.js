504aa55525cee3b4716bcc14bcaa6344cd359813
guaaaaa <63753679+guaaaaa@users.noreply.github.com>
2025-01-18 21:15:08 -0500

    Update constants.js

--- hackathon_site/dashboard/frontend/src/constants.js
+++ hackathon_site/dashboard/frontend/src/constants.js
@@ -1,9 +1,9 @@
-export const hackathonName = "MakeUofT 2024";
+export const hackathonName = "MakeUofT 2025";
 export const adminGroup = "Hardware Site Admins";
 export const minTeamSize = 2;
 export const maxTeamSize = 4;
-export const hardwareSignOutStartDate = new Date(2024, 1, 18, 6, 30);
-export const hardwareSignOutEndDate = new Date(2024, 1, 18, 11, 0);
+export const hardwareSignOutStartDate = new Date(2025, 1, 17, 6, 30);
+export const hardwareSignOutEndDate = new Date(2025, 2, 18, 11, 0);
 export const hssTestUserGroup = "HSS Test Users";
 export const serverUrl = "https://makeuoft.ca";
 export const minProjectDescriptionLength = 60;
